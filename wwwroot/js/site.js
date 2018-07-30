22aed77eac45da8ed592aa9b301c177fe561d678
LuisPablo <luispablo.hg@gmail.com>
2018-07-30 12:25:55 -0500

    Init

---
+++ wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function name(params) {
+    console.log('r');
+    
+    // This function rocks
+
+    // This function rocks
+    
+    function name(){
+        return value
+    }
+}
+
